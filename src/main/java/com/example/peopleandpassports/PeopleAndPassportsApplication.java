aa6554c11aca872eb8b7acb33d38c73c063afb0d
H.M.Dezső <hertermiklosdezso@gmail.com>
2023-02-11 16:43:31 +0100

    Initial commit

---
+++ src/main/java/com/example/peopleandpassports/PeopleAndPassportsApplication.java
@@ -0,0 +1,13 @@
+package com.example.peopleandpassports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleAndPassportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleAndPassportsApplication.class, args);
+	}
+
+}
